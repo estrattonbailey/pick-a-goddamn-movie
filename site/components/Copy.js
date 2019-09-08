bbbc14a7ec7ec156a089a5f0c3b8a944ee72a3ac
yomelaniejo <melaniejslattery@gmail.com>
2019-09-08 19:46:47 -0400

    update copy

--- site/components/Copy.js
+++ site/components/Copy.js
@@ -10,13 +10,15 @@ export default props => (
         <div className='copy__left'>
           <h4>No seriously, what’s the point?</h4>
           <p>Ever behind on all-things entertainment,  Melanie and Eric waste hours browsing the latest content diarrhea spewing from today’s streaming platforms.  And yet, most evenings (after sifting through 2-27 trailers), the pitiful pair won’t wind up watching anything.</p>
-          <p>Thus, the duo decided to create a new, fail-safe system. If they limit their options to a measly six movies, they'll, like, HAVE to select one. In other words, they are not allowed to watch ANY other movies than the chosen six.</p>
+          <p>Thus, the duo decided to create a new, fail-safe system. If they limit their options to a measly six movies, they'll, like, HAVE to select one.</p>
+          <p>In other words, they are not allowed to watch ANY other movies than the six you see above.</p>
         </div>
         <div className='copy__right'>
           <h4>What are the parameters?</h4>
           <p>At the onset, both participants select a maximum of THREE movies to contribute to the selection pool. Anytime the two gather to watch a movie, they can only select a film from the established six. NO EXCEPTIONS.</p>
           <p>Participants may NOT switch a movie title after it has been submited. After a participant’s submission has been viewed, they must submit a new film to fill their open slot. Repeat submissions are permitted.</p>
           <p>If a single participant’s movie submissions have been viewed three times in a row, the pair MUST choose a film from the other participant’s line-up.</p>
+          <p>*Disclaimer: Assume most of Eric's selections are because <em>Melanie</em> hasn't seen the movie.</p>
         </div>
 
         <hr />
