f79eebc20b851834c999c96c39d3fe5f9d32e7f9
yomelaniejo <melaniejslattery@gmail.com>
2019-09-08 19:51:59 -0400

    update copy

--- site/components/Copy.js
+++ site/components/Copy.js
@@ -11,13 +11,13 @@ export default props => (
           <h4>No seriously, what’s the point?</h4>
           <p>Ever behind on all-things entertainment,  Melanie and Eric waste hours browsing the latest content diarrhea spewing from today’s streaming platforms.  And yet, most evenings (after sifting through 2-27 trailers), the pitiful pair won’t wind up watching anything.</p>
           <p>Thus, the duo decided to create a new, fail-safe system. If they limit their options to a measly six movies, they'll, like, HAVE to select one.</p>
-          <p>In other words, they are not allowed to watch ANY other movies than the six you see above.</p>
+          <p>In short, they are not allowed to watch ANY movies other than the six you see above.</p>
         </div>
         <div className='copy__right'>
           <h4>What are the parameters?</h4>
           <p>At the onset, both participants select a maximum of THREE movies to contribute to the selection pool. Anytime the two gather to watch a movie, they can only select a film from the established six. NO EXCEPTIONS.</p>
-          <p>Participants may NOT switch a movie title after it has been submited. After a participant’s submission has been viewed, they must submit a new film to fill their open slot. Repeat submissions are permitted.</p>
-          <p>If a single participant’s movie submissions have been viewed three times in a row, the pair MUST choose a film from the other participant’s line-up.</p>
+          <p>Participants may NOT switch a movie title after it has been submited. After a participant’s submission has been viewed, they must submit a new film to fill their open slot. Repeat submissions ARE permitted.</p>
+          <p>If an individual’s submissions have been selected three times in a row, the pair MUST choose a film from the other contributor’s line-up.</p>
           <p><strong>Disclaimer:</strong> A bulk of Eric's selections are because <em>Melanie</em> hasn't seen the movie.</p>
         </div>
 
